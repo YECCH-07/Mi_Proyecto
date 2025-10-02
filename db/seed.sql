e0405d2a3010cac802209e74b11b3e8f193e82aa
Celiii779 <221950@unsaac.edu.pe>
2025-10-01 20:53:41 -0500

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,6 @@
+--Insertando
 -- Seeds de ejemplo
+
 INSERT INTO usuarios (nombre, email, password_hash, rol) VALUES
 ('Admin','admin@example.com','$2b$10$examplehash','autoridad'),
 ('Juan Perez','juan@example.com','$2b$10$examplehash','ciudadano');
