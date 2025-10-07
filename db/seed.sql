f687dd27c8813bb1b7929a8aa23a2676d4da1bae
Medaly <195050@unsaac.edu.pe>
2025-10-06 22:03:57 -0500

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -4,3 +4,4 @@
 INSERT INTO usuarios (nombre, email, password_hash, rol) VALUES
 ('Admin','admin@example.com','$2b$10$examplehash','autoridad'),
 ('Juan Perez','juan@example.com','$2b$10$examplehash','ciudadano');
+('Juliana Huaman','juliana@example.com','$2b$10$examplehash','ciudadana');
